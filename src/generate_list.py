823e5a82ea0de024b2a781b3ec71233efa38bbcf
kujirapars <ku.jirapars_st@tni.ac.th>
2017-02-03 09:51:30 +0700

    Update generate_list.py

--- src/generate_list.py
+++ src/generate_list.py
@@ -1,6 +1,11 @@
 import random
 def generate_list():
     alist = [x for x in range(random.randint(-10,10))]
+    sum = 0
+    for i in alist:
+        sum += alist[i]
+    assert (alist != []),"List is empty"
+    assert (sum >= 0),"Sum's list is less than 0"
     return alist
     
 def printIt():
